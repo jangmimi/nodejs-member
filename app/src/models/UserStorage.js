399b00544558d5d6bc32c9bcb8cb10b586646b5f
jangmimi <alwkd0101@naver.com>
2023-10-15 23:16:39 +0900

    User 모델 만들기 인스턴스화

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -17,6 +17,18 @@ class UserStorage {
         }, {});
         return newUsers;
     }
+
+    static getUserInfo(id) {
+        const users = this.#users;
+        const idx = users.id.indexOf(id);
+        const usersKeys = Object.keys(users);   // [id, pw, name] 배열 생성
+        const userInfo = usersKeys.reduce((newUser, info) => {
+            newUser[info] = users[info][idx];
+            return newUser;
+        }, {});
+
+        return userInfo;
+    }
 }
 
 module.exports = UserStorage;

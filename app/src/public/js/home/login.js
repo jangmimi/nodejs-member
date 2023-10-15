4350a516330a9b7c782a0edb80fc95c05e2c997a
jangmimi <alwkd0101@naver.com>
2023-10-15 21:07:35 +0900

    DOM으로 HTML객체 제어-프론트

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,20 @@
 "use strict"
 
-console.log("hello");
-console.log("bye");
+const id = document.querySelector("#id"),
+    pw = document.querySelector("#pw"),
+    loginBtn = document.querySelector("button");
+
+console.log(id);
+console.log("Hello");
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id: id.value,
+        pw: pw.value,
+    };
+    console.log(req);
+}
+
+// DOM -> Document Object Model

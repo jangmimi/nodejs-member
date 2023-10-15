6d973b0b3a7f3aa4009c92001b23c5d3226abf44
jangmimi <alwkd0101@naver.com>
2023-10-15 23:18:19 +0900

    User 모델 만들기 인스턴스화

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,6 +1,5 @@
 "use strict";
 
-const UserStorage = require("../../models/UserStorage");
 const User = require("../../models/User");
 
 const output = {
@@ -16,7 +15,7 @@ const process = {
     login: (req, res) => {
         const user = new User(req.body);
         const response = user.login();
-        console.log(response);
+        return res.json(response);
     },
 };
 

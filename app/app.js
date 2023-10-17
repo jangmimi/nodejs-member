11411edba9c98d56b098d6f4e41a822836635417
jangmimi <alwkd0101@naver.com>
2023-10-17 10:59:13 +0900

    로그 관리 모듈 winston 등록 및 설정

--- app/app.js
+++ app/app.js
@@ -4,9 +4,12 @@
 const express = require('express');
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
-dotenv.config( {path: '.env'} );
+// const morgan = require("morgan");
 
 const app = express();
+dotenv.config( {path: '.env'} );
+
+// const accessLogStream = require("./src/config/log");
 
 // 라우팅
 const home = require("./src/routes/home");
@@ -15,8 +18,12 @@ const home = require("./src/routes/home");
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 app.use(express.static(`${__dirname}/src/public`));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.use(morgan('dev'));
+// app.use(morgan('common', { stream: accessLogStream }));
 
 app.use("/", home);  // use -> 미들 에어를 등록하주는 메서드
 
--- app/src/config/db.js
+++ app/src/config/db.js
@@ -1,4 +1,4 @@
-'use strict';  
+"use strict"
 
 const mysql = require("mysql");
 
